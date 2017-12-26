1f8d4eebe68862fdfa5cc311524d0a935ed6b915
zhangjingf <734546749@qq.com>
2017-12-26 15:09:09 +0800

    修改配置

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vueandts
-  a vue and typescript framework
+  本项目支持在vue框架中使用typescript写法，同时集承webpack模块化打包，支持scss,支持雪碧图生成，并包含一些开发中常用的功能组件。
+  1.安装依赖包
+    npm install
+  2.本地开发
+    npm start
---
+++ src/views/index/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="m-index">
+        <p>m-index</p>
+    </div>
+</template>
+<script>
+export default {
+    props: ["options"],
+    data() {
+
+    },
+    methods: {
+        
+    },
+    mounted() {
+        const vm = this;
+        console.log(vm);
+    }
+} 
+</script>
+<style lang="scss" scoped>
+
+</style>
