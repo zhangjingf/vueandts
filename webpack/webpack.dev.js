b664ae37be5cddb37a8e0708dfc92c4e74d65239
zhangjingf <734546749@qq.com>
2018-06-09 20:10:33 +0800

    增加代码

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const path = require("path");
+const express = require("express");
+var app = express();
+app.use(express.static('dist'));
+var server = app.listen(9000);
 module.exports = merge(common, {
     devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist/index'
-    }
+    // devServer: {
+    //     contentBase: path.join(__dirname, "../dist"),
+    //     compress: true,
+    //     port: 9000
+    // }
 });
